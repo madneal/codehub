bcbe52919bddc5a6cb504c8d417523078cd4d168
neal1991 <neal1991@sina.com>
2017-12-24 15:30:13 +0800

    add upload

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,9 +5,19 @@ const app = getApp()
 Page({
   data: {
     motto: 'Hello World',
+    codeList: [],
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  // add new Code
+  navigateNewCode: function() {
+    wx.navigateTo({
+      url: '../upload/index',
+      sucess: function(res) {
+        console.log(res.data);
+      }
+    })
   },
   //事件处理函数
   bindViewTap: function() {
